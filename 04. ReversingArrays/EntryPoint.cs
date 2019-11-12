4052ad3f58b89a5f2bbccb5e0b7209f65749a705
Lockeheede <prescott.lawani@gmail.com>
2019-11-12 14:59:27 -0600

    Adding Array.Reverse(); method. Use this to change the order of methods. There is a try of changing the index and length of a reverse method in its own personal method, but it might not have worked...will try to fix it later.

---
+++ 04. ReversingArrays/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+    class EntryPoint
+    {
+        static void Main()
+        {
+        string[] wuTang = { "ghostface", "methodman", "rza", "gza", "raekwon", "odb" };
+
+        //Using the reverse method takes the elements and changes the order
+        //Array.Reverse(wuTang);
+        //You can also start at an index and go for a specific length
+        //Array.Reverse(wuTang, 1, 3); //means you start at methodman and end at raekwon
+
+        Array.Reverse(wuTang);
+
+        foreach (var item in wuTang)
+        {
+            Console.WriteLine(item);
+        }
+        }
+
+    static void Reverse(string[] array, int index, int length)
+    {
+        string temp = string.Empty;
+
+        for (int i = index; i < length / 2; i++)
+        {
+            temp = array[i];
+            array[i] = array[length - i - 1];
+            array[length - i - 1] = temp;
+        }
+    }
+    }
+
