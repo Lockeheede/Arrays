59472e0cb6c9136b9b3d92834a2b4e8350bfcdda
Lockeheede <prescott.lawani@gmail.com>
2019-11-26 21:42:28 -0600

    Added symmetry and list introduction

---
+++ 08. Symmetry/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+    ///Array symmetry works where you split an array down the middle
+    ///And you match the mirror elements to see if they are equal
+    ///This can be done with an odd number of indexes as well
+    ///1 2 3 4 4 3 2 1
+    ///1 2 3 4 3 2 1 <--Here you would just split it down the four
+
+    int[] symmetryArray = { 1, 2, 3, 4, 3, 2, 1 };
+
+    bool isSymmetrical = true;
+
+        for (int i = 0; i < symmetryArray.Length / 2; i++)
+        {
+            if (symmetryArray[i] != symmetryArray[symmetryArray.Length - i - 1])
+            {
+                isSymmetrical = false;
+                break;
+            }
+        }
+            if (isSymmetrical == true)
+            {
+                Console.WriteLine("The array is symmetrical!");
+            }
+            else
+            {
+                Console.WriteLine("The array is not symmetrical!");
+            }
+    }
+}
+
+
---
+++ 09. Lists/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+    class EntryPoint
+    {
+        static void Main()
+        {
+        List<int> thisList = new List<int>();
+        ///Different than arrays. For instance, you don't
+        ///Define the indeces. And, if you use thisList[8] = 7; 
+        ///You WON'T get an error until AFTER the code is run
+
+        ///Instead you use:
+        thisList.Add(69);//which adds a single element with the value of 69
+
+        ///You can remove specific elements with:
+        thisList.Remove(69);
+
+
+        thisList.Add(2);
+        thisList.Add(4);
+        thisList.Add(6);
+        thisList.Add(8);
+
+        //Or a specific index with:
+        thisList.RemoveAt(0);
+
+        Console.WriteLine(thisList[0]);
+
+        //You can use .count to count the arrays
+        Console.WriteLine(thisList.Count);
+
+        //Lists do not have a specific, fixed size like an array
+        //Use Lists when you are unsure of how many indeces you'll be using in the array
+        }
+    }
+
